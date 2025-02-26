5672bdc708a3eb397aea43b0b953bf9b13acd1d2
LuizGabrielTeixeira <1230350@isep.ipp.pt>
2025-02-26 19:46:04 +0000

    exercise I

--- semana8/src/ExI.java
+++ semana8/src/ExI.java
@@ -1,49 +1,66 @@
-import java.utils;
+import java.util.Scanner;
 
 public class ExI {
-	public static void main (String[] Args){
-		condominium();
-	}
+    public static void main(String[] Args) {
+        condominium();
+    }
 
-	public static void condominium (){
-	Scanner sc = new Scanner(System.in);
-	
-	String[][] condominium = new String[3][4];
-	
-	String names;
-	String nameForSearch;
+    public static void condominium() {
+        Scanner sc = new Scanner(System.in);
 
-	for (int i = 0; i < condominium[0].lenght; i++){
-		for (int j = 0; j < condominium.lenght; j++){
-			names = sc.next();
-			condominium[i][j] = names;	
-		}
-	}
+        String[][] condominium = new String[4][3];
 
-	nameForSearch = sc.next();
-	
-	searchName(condominium, nameForSearch);
-	}
+        String name;
+        String nameForSearch;
 
-	public static void searchName(condominium, nameForSearch){
-	int entrance;
-	int floor;
-	boolean nameFinded;
+        for (int j = 0; j < condominium[0].length; j++) {
+            for (int i = 0; i < condominium.length; i++) {
+                name = sc.nextLine();
+                condominium[i][j] = name;
+            }
+        }
 
-	for(int i = 0; i < condominium[0].lenght; i++){
-		for (int j = 0; condominium.lenght; j++){
-			String actualName = condominium[i][j];
-			if (actualName.equals(nameForSearch)){
-			entrance = j;
-			floor = i;
-			nameFinded = true;
-			}
-		}
-		if (nameFinded){
-		System.out.println("name=" + nameForSearch);
-		System.out.println("entrance=" + entrance);
-		System.out.println("floor=" + floor)
-		}
-	}
-	}
+        nameForSearch = sc.nextLine();
+
+        boolean[][] founds = searchName(condominium, nameForSearch);
+        printFounds(founds, condominium);
+
+        sc.close();
+    }
+
+    public static void printFounds(boolean[][] founds, String[][] condominium) {
+        boolean noOneFound = true;
+
+        for (int j = 0; j < condominium[0].length; j++) {
+            for (int i = 0; i < condominium.length; i++) {
+                if (founds[i][j]) {
+                    System.out.println("name=" + condominium[i][j]);
+                    System.out.println("entrance=" + j);
+                    System.out.println("floor=" + i);
+                    noOneFound = false;
+                    break;
+                }
+            }
+        }
+        if (noOneFound) {
+            System.out.println("Do not live in the building");
+        }
+
+    }
+
+    public static boolean[][] searchName(String[][] condominium, String nameForSearch) {
+        boolean[][] founds = new boolean[4][3];
+
+        for (int j = 0; j < condominium[0].length; j++) {
+            for (int i = 0; i < condominium.length; i++) {
+                String actualName = condominium[i][j];
+                if (actualName.equalsIgnoreCase(nameForSearch)) {
+                    founds[i][j] = true;
+                    break;
+                }
+            }
+        }
+        return founds;
+    }
 }
+
