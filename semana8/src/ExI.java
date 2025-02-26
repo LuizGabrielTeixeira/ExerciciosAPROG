59ee4b8ee9c611a1bea91c279ee5d58af59c99b8
Gabriel Sargaço Teixeira <luizgabriellgsst@gmail.com>
2025-02-26 12:54:39 +0000

    Exercise I, not completed

--- semana8/src/ExI.java
+++ semana8/src/ExI.java
@@ -1,2 +1,49 @@
+import java.utils;
+
 public class ExI {
+	public static void main (String[] Args){
+		condominium();
+	}
+
+	public static void condominium (){
+	Scanner sc = new Scanner(System.in);
+	
+	String[][] condominium = new String[3][4];
+	
+	String names;
+	String nameForSearch;
+
+	for (int i = 0; i < condominium[0].lenght; i++){
+		for (int j = 0; j < condominium.lenght; j++){
+			names = sc.next();
+			condominium[i][j] = names;	
+		}
+	}
+
+	nameForSearch = sc.next();
+	
+	searchName(condominium, nameForSearch);
+	}
+
+	public static void searchName(condominium, nameForSearch){
+	int entrance;
+	int floor;
+	boolean nameFinded;
+
+	for(int i = 0; i < condominium[0].lenght; i++){
+		for (int j = 0; condominium.lenght; j++){
+			String actualName = condominium[i][j];
+			if (actualName.equals(nameForSearch)){
+			entrance = j;
+			floor = i;
+			nameFinded = true;
+			}
+		}
+		if (nameFinded){
+		System.out.println("name=" + nameForSearch);
+		System.out.println("entrance=" + entrance);
+		System.out.println("floor=" + floor)
+		}
+	}
+	}
 }
