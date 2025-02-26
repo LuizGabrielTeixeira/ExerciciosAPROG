f0f59e7aaa3159c27900ec7481309af7f913e9dc
LuizGabrielTeixeira <1230350@isep.ipp.pt>
2025-02-26 01:38:46 +0000

    exercise H

--- semana8/src/ExG.java
+++ semana8/src/ExG.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class ExG {
+    public static void main(String[] args) {
+        invertSequence();
+    }
+
+    public static void invertSequence (){
+        Scanner sc = new Scanner(System.in);
+
+        int quantityOfNumbers = sc.nextInt();
+        int[] numbers = new int[quantityOfNumbers];
+
+        for (int i = 0; i < quantityOfNumbers; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        invertAndPrintSequence(numbers);
+    }
+
+    public static void invertAndPrintSequence(int[] numbers){
+        for (int i = numbers.length -1; i > -1 ; i--) {
+            System.out.printf("%d\n", numbers[i]);
+        }
+    }
 }
--- semana8/src/ExH.java
+++ semana8/src/ExH.java
@@ -1,2 +1,74 @@
+import java.util.Scanner;
+
 public class ExH {
+    public static void main(String[] args) {
+        reedSequence();
+    }
+
+    public static void reedSequence() {
+        Scanner sc = new Scanner(System.in);
+
+        int quantityOfNumbers = sc.nextInt();
+
+        int[] sequence = new int[quantityOfNumbers];
+
+        for (int i = 0; i < quantityOfNumbers; i++) {
+            sequence[i] = sc.nextInt();
+        }
+
+        reedCommands(sc, sequence);
+
+        sc.close();
+    }
+
+    public static void reedCommands(Scanner sc, int[] sequence) {
+        String command;
+
+        do {
+            command = sc.next();
+            if (command.equalsIgnoreCase("right")) {
+                rotateRight(sequence);
+            } else if (command.equalsIgnoreCase("left")) {
+                rotateLeft(sequence);
+            }
+        } while (!command.equalsIgnoreCase("exit"));
+    }
+
+    public static void rotateRight(int[] sequence) {
+        int lastNumber = sequence[sequence.length - 1];
+        int temporaryNumberA = sequence[0];
+        int temporaryNumberB;
+
+        sequence[0] = lastNumber;
+
+        for (int i = 1; i < sequence.length; i++) {
+            temporaryNumberB = sequence[i];
+            sequence[i] = temporaryNumberA;
+            temporaryNumberA = temporaryNumberB;
+        }
+
+        for (int i = 0; i < sequence.length; i++) {
+            System.out.printf("[%d] ", sequence[i]);
+        }
+        System.out.println();
+    }
+
+    public static void rotateLeft(int[] sequence) {
+        int firstNumber = sequence[0];
+        int temporaryNumberA = sequence[sequence.length - 1];
+        int temporaryNumberB;
+
+        sequence[sequence.length - 1] = firstNumber;
+
+        for (int i = sequence.length -2; i > -1; i--) {
+            temporaryNumberB = sequence[i];
+            sequence[i] = temporaryNumberA;
+            temporaryNumberA = temporaryNumberB;
+        }
+
+        for (int i = 0; i < sequence.length; i++) {
+            System.out.printf("[%d] ", sequence[i]);
+        }
+        System.out.println();
+    }
 }
