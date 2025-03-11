37a13facae6f31d182d21a9274fb757aba297bbf
Gabriel Sargaço Teixeira <luizgabriellgsst@gmail.com>
2025-03-11 00:08:20 +0000

    Exercises J, K and L

--- semana9/src/ExK.java
+++ semana9/src/ExK.java
@@ -1,2 +1,62 @@
+import java.util.Scanner;
+
 public class ExK {
+    public static void main(String[] args) {
+        noRepetitions();
+    }
+
+    public static void noRepetitions() {
+        Scanner sc = new Scanner(System.in);
+
+        int quantityOfNumbers = sc.nextInt();
+
+        int number;
+        int[] numbers = new int[quantityOfNumbers];
+
+        for (int i = 0; i < quantityOfNumbers; i++) {
+            number = sc.nextInt();
+            numbers[i] = number;
+        }
+
+        int[] withNoRepetition = checkRepetition(numbers);
+        printNumbers(withNoRepetition);
+
+        sc.close();
+    }
+
+    public static int[] checkRepetition(int[] numbers) {
+        int[] withNoRepetition = new int[numbers.length];
+        int count = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            withNoRepetition[i] = Integer.MIN_VALUE;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            boolean alreadyExists = false;
+            for (int j = 0; j < count; j++) {
+                if (numbers[i] == withNoRepetition[j]) {
+                    alreadyExists = true;
+                    break;
+                }
+            }
+            if (!alreadyExists) {
+                withNoRepetition[count] = numbers[i];
+                count++;
+            }
+        }
+
+        return withNoRepetition;
+    }
+
+    public static void printNumbers (int[] withNoRepetition) {
+        for (int i = 0; i < withNoRepetition.length; i++) {
+            if (withNoRepetition[i] != Integer.MIN_VALUE) {
+                System.out.println(withNoRepetition[i]);
+            }
+            else {
+                break;
+            }
+        }
+    }
 }
--- semana9/src/ExL.java
+++ semana9/src/ExL.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class ExL {
+    public static void main(String[] args) {
+        neighbours();
+    }
+
+    private static void neighbours() {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[1000];
+        int counter = 0;
+
+        for (int i = 0; - 1 < numbers[i]; i++) {
+            numbers[i] = sc.nextInt();
+            if (numbers[i] < 0) {
+                break;
+            } else {
+                counter++;
+            }
+        }
+
+        printLargerThanNeighbours(numbers, counter);
+
+        sc.close();
+    }
+
+    public static void printLargerThanNeighbours (int[] numbers, int counter) {
+        for (int i = 1; i < counter - 1; i++) {
+            if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1]) {
+                System.out.println(numbers[i]);
+            }
+        }
+    }
 }
